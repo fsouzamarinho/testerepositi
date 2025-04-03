414925c81d07daa04c65b4e3bc264a0b2f56cf00
Fabiano Marinho <siteandravirtual@gmail.com>
2025-04-03 09:18:24 -0300

    Slice

--- main.go
+++ main.go
@@ -2,11 +2,27 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	var ages = [4]int{17, 16, 20, 40}
 	nomes := [4]string{"Mario", "Luigi", "DeadPool", "Superman"}
 	fmt.Println(ages)
 	fmt.Println(nomes)
 	nomes[3] = "Clark Kent"
-	fmt.Println(nomes)	
+	fmt.Println(nomes)
+	// Slice
+	var score = []int{100, 200, 300, 400}
+	fmt.Println(score)
+	score[1] = 2
+	fmt.Println(score, len(score), cap(score))
+	rangeOne := score[1:3]
+	fmt.Println(rangeOne)
+	rangeTwo := score[2:]
+	fmt.Println(rangeTwo)
+	rangeThree := score[:3]
+	fmt.Println(rangeThree)
+
+	var superherois = []string{"Deadpool", "Homem-aranha", "Motoqueiro Fantasma"}
+	fmt.Println(superherois)
+	superherois = append(superherois, "Ben 10", "Mulher Maravilha")
+	fmt.Println(superherois, len(superherois), cap(superherois))
 }
